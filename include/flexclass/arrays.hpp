70c8dc742ed9084b17742b27152dc644e1995a75
Breno Guimaraes <brenorg@gmail.com>
2020-09-19 09:28:10 -0400

    Extract array library from core infrastructure

---
+++ include/flexclass/arrays.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "flexclass.hpp"
+
+namespace fc
+{
+
+template<class T> struct UnsizedArray
+{
+    UnsizedArray(ArrayBuilder<T>&& aph) : m_begin(aph.begin()) {}
+
+    using type = T;
+    using fc_array_kind = unsized;
+    static constexpr auto array_alignment = alignof(T);
+
+    template<class Derived>
+    auto begin(const Derived* ptr) const { return m_begin; }
+
+    auto begin() const { return m_begin; }
+
+    T* const m_begin;
+};
+
+template<class T> struct SizedArray
+{
+    SizedArray(ArrayBuilder<T>&& aph) : m_begin(aph.begin()), m_end(aph.end()) {}
+
+    using type = T;
+    using fc_array_kind = sized;
+    static constexpr auto array_alignment = alignof(T);
+
+    template<class Derived>
+    auto begin(const Derived* ptr) const { return m_begin; }
+
+    template<class Derived>
+    auto end(const Derived* ptr) const { return m_end; }
+
+    auto begin() const { return m_begin; }
+    auto end() const { return m_end; }
+
+    T* const m_begin;
+    T* const m_end;
+};
+
+template<class T, int El = -1> struct AdjacentArray
+{
+    AdjacentArray(ArrayBuilder<T>&&) {}
+
+    using type = T;
+    using fc_array_kind = unsized;
+    static constexpr auto array_alignment = alignof(T);
+
+    template<class Derived>
+    auto begin(const Derived* ptr) const
+    {
+        if constexpr (El == -1)
+            return aligner(ptr).advance(1).template get<T>();
+        else
+            return aligner(ptr->template end<El>()).template get<T>();
+    }
+};
+
+template<class T, int El = -1> struct SizedAdjacentArray
+{
+    SizedAdjacentArray(ArrayBuilder<T>&& aph) : m_end(aph.end()) {}
+
+    using type = T;
+    using fc_array_kind = sized;
+    static constexpr auto array_alignment = alignof(T);
+
+    template<class Derived>
+    auto begin(const Derived* ptr) const
+    {
+        if constexpr (El == -1)
+            return aligner(ptr).advance(1).template get<T>();
+        else
+            return aligner(ptr->template end<El>()).template get<T>();
+    }
+
+    template<class Derived>
+    auto end(const Derived* ptr) const { return m_end; }
+
+    T* const m_end;
+};
+
+template<class T>
+struct ArraySelector<T[]>
+{
+    using type = std::conditional_t< std::is_trivially_destructible<T>::value
+                                     , UnsizedArray<T>
+                                     , SizedArray<T>
+                                     >;
+};
+
+}
